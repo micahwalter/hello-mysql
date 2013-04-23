473c9bca3eb6576ecc8ee0fe8e2c9da8199e97ef
Micah Walter <micah@micahwalter.com>
2013-04-22 20:32:44 -0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+hello-mysql
+===
+
+Just my notes on setting up a python/flask app with mysql, on RDS and Heroku...
---
+++ hello-mysql.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_USER']
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+app.config['MYSQL_DATABASE_HOST'] = os.environ['MYSQL_HOST']
+app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DB']
+
+mysql.init_app(app)
+
+@app.route('/')
+def hello():
+	cursor = mysql.get_db().cursor()
+	
+	return 'Hello World!'
